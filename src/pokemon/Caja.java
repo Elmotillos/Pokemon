45f293b6d674db2ca8fbae3e8ea173ec9f7ac5cf
Miguel Zuza Cano <mizuzaca@gmail.com>
2022-05-05 10:58:22 +0200

    Clase Caja terminada

---
+++ src/pokemon/Caja.java
@@ -0,0 +1,20 @@
+package pokemon;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Caja {
+
+    private List<ClasePokemon>  listPokemon;
+
+    public Caja() {
+        this.listPokemon = new LinkedList<ClasePokemon>();
+    }
+
+    public Caja(ClasePokemon pokemon) {
+        this.listPokemon = listPokemon;
+    }
+
+    
+    
+}
